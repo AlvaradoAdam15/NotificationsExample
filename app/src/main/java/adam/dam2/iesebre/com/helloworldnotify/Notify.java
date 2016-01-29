669312f40e9c51d26e57017c6e14b9ca9b3eb5d0
AlvaradoAdam15 <adamalvarado@iesebre.com>
2016-01-29 17:25:36 +0100

    Add log in Notify

--- app/src/main/java/adam/dam2/iesebre/com/helloworldnotify/Notify.java
+++ app/src/main/java/adam/dam2/iesebre/com/helloworldnotify/Notify.java
@@ -1,8 +1,11 @@
 package adam.dam2.iesebre.com.helloworldnotify;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.onesignal.OneSignal;
+
+import org.json.JSONObject;
 
 /**
  * Created by adam on 29/01/16.
@@ -12,5 +15,21 @@ public class Notify extends Application {
     public void onCreate() {
         super.onCreate();
         OneSignal.startInit(this).init();
+    }
+
+    private class ExampleNotificationOpenedHandler implements OneSignal.NotificationOpenedHandler {
+        @Override
+        public void notificationOpened(String message, JSONObject additionalData, boolean isActive) {
+            try {
+                if (additionalData != null) {
+                    if (additionalData.has("actionSelected"))
+                        Log.d("OneSignalExample", "OneSignal notification button with id " + additionalData.getString("actionSelected") + " pressed");
+
+                    Log.d("OneSignalExample", "Full additionalData:\n" + additionalData.toString());
+                }
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+        }
     }
 }
